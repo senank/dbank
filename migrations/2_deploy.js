161acec8ca2d2d58bf2913231ec10af23bdaeee8
senan <senankassem@gmail.com>
2021-04-28 20:37:03 -0400

    pushing token contracts to blockchain

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,14 +1,22 @@
 const Token = artifacts.require("Token");
 const dBank = artifacts.require("dBank");
+// importing smart contracts as javascript
+// these would be converted into the abis (better called artifacts)
+// moves smartcontract from computer to blockchain
+// file ordered for running orders
 
 module.exports = async function(deployer) {
-	//deploy Token
-
+	//deploy Token - truffle deployer
+	await deployer.deploy(Token)
 	//assign token into variable to get it's address
+	const token = await Token.deployed()
 	
 	//pass token address for dBank contract(for future minting)
+	await deployer.deploy(dBank, token.address)
 
 	//assign dBank contract into variable to get it's address
+	const dbank = await dBank.deployed()
 
 	//change token's owner/minter from deployer to dBank
+	await token.passMinterRole(dbank.address)
 };
